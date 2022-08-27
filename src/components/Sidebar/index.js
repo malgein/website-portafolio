9e4dba369497699049112485271e0d49347402b3
wilmer pocaterra <malgein17@gmail.com>
2022-08-26 21:17:38 -0400

    uploading the basic

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,49 @@
+import './index.scss'
+import {Link, NavLink} from 'react-router-dom'
+import LogoW from '../../assets/images/vecteezy_w-letter-business-logo-design-vector_.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faUser,} from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin, faSkype } from '@fortawesome/free-brands-svg-icons'
+
+const Sidebar = () =>{
+	return(
+		<div className="nav-bar">
+			<Link className="logo" to='/'>
+				<img src={LogoW} alt="logo" />
+				<p>Wilmer</p>
+			</Link>
+			<nav>
+				<NavLink exact="true" activeclassname="active" to="/">
+					<FontAwesomeIcon icon={faHome} color="#4d4d4c" />
+				</NavLink>
+				<NavLink exact="true" activeclassname="active" to="/about"
+					className="about-link">
+					<FontAwesomeIcon icon={faUser} color="#4d4d4c" />
+				</NavLink>
+				<NavLink exact="true" activeclassname="active" to="/contact"
+					className="contact-link">
+					<FontAwesomeIcon icon={faEnvelope} color="#4d4d4c" />
+				</NavLink>
+			</nav>
+				<ul>
+					<li>
+						<a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/wilmerpocaterra/">
+							<FontAwesomeIcon icon={faLinkedin} color="4d4d4e" />
+						</a>
+					</li>
+					<li>
+						<a target="_blank" rel="noreferrer" href="https://github.com/malgein">
+							<FontAwesomeIcon icon={faGithub} color="4d4d4e" />
+						</a>
+					</li>
+					<li>
+						<a target="_blank" rel="noreferrer" href="https://join.skype.com/invite/oUFdpU9UhrUb">
+							<FontAwesomeIcon icon={faSkype} color="4d4d4e" />
+						</a>
+					</li>
+				</ul>
+		</div>
+	)
+}
+
+export default Sidebar
