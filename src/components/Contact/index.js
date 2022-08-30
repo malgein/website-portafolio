301d40f7c3a20fee898e2dc3d81246e05582dddb
wilmer pocaterra <malgein17@gmail.com>
2022-08-30 10:22:10 -0400

    adding a contact again

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,15 +1,16 @@
-import {useState } from 'react'
+import { useEffect, useState } from 'react'
 import Loader from 'react-loaders'
-import AnimatedLetters from '../AnimatedLetters'
-import './index.scss'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import { useRef } from 'react'
 import emailjs from '@emailjs/browser'
+import AnimatedLetters from '../AnimatedLetters'
+import './index.scss'
 
 const Contact = () => {
-
-	const [letterClass, setLetterClass] = useState('text-animate text-animate-hover')
+  const [letterClass, setLetterClass] = useState('text-animate')
   const form = useRef()
 
+ 
   const sendEmail = (e) => {
     e.preventDefault()
 
@@ -25,10 +26,10 @@ const Contact = () => {
         }
       )
   }
-	
-	return(
-		<>
-			<div className="container contact-page">
+
+  return (
+    <>
+      <div className="container contact-page">
         <div className="text-zone">
           <h1>
             <AnimatedLetters
@@ -37,12 +38,12 @@ const Contact = () => {
               idx={15}
             />
           </h1>
-					<p>
-					I am interested in freelance opportunities - especially on ambitious
+          <p>
+            I am interested in freelance opportunities - especially on ambitious
             or large projects. However, if you have any other requests or
             questions, don't hesitate to contact me using below form either.
-					</p>
-					<div className="contact-form">
+          </p>
+          <div className="contact-form">
             <form ref={form} onSubmit={sendEmail}>
               <ul>
                 <li className="half">
@@ -77,11 +78,29 @@ const Contact = () => {
               </ul>
             </form>
           </div>
-				</div>
-			</div>
-			<Loader type="pacman" />
-		</>
-	)
+        </div>
+        <div className="info-map">
+          Slobodan Gajić,
+          <br />
+          Serbia,
+          <br />
+          Branka RadiČevića 19, 22000 <br />
+          Sremska Mitrovica <br />
+          <br />
+          <span>freelancerslobodan@gmail.com</span>
+        </div>
+        <div className="map-wrap">
+          <MapContainer center={[44.96366, 19.61045]} zoom={13}>
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <Marker position={[44.96366, 19.61045]}>
+              <Popup>Sloba lives here, come over for a cup of coffee :)</Popup>
+            </Marker>
+          </MapContainer>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
 }
 
 export default Contact
